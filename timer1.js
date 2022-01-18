37268eefe3de8c489961e3445edd7bf4878d5182
Seamus McGill <seamusmcgill@gmail.com>
2022-01-18 12:15:53 -0500

    Created a program that beeps given time in seconds via the command line

---
+++ timer1.js
@@ -0,0 +1,21 @@
+// Create a function to get relevant arguments from command line
+const getBeepTimes = (args) => {
+  let beepTimes = [];
+  for (let i = 2; i < args.length; i++) {
+    // Convert string to a number
+    let parsedNumber = parseInt(args[i]);
+    // Check that argument is positive and a number
+    if (!isNaN(parsedNumber) && parsedNumber >= 0) {
+      beepTimes.push(args[i] * 1000);
+    }
+  }
+  return beepTimes;
+};
+
+let beepTimes = getBeepTimes(process.argv);
+// Loop through beeptimes and output sound after delay
+for (const time in beepTimes) {
+  setTimeout(() => {
+    process.stdout.write("\x07");
+  }, time);
+}
